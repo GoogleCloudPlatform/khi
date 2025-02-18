77082a4300be8fb27348dcdaa6b6a982cb66a212
Junya Okabe <86868255+Okabe-Junya@users.noreply.github.com>
2025-02-18 17:07:30 +0900

    update: internal/testflags/testflags.go
    Co-authored-by: kyasbal <kyasbal1994@gmail.com>
    Signed-off-by: Junya Okabe <86868255+Okabe-Junya@users.noreply.github.com>

--- internal/testflags/testflags.go
+++ internal/testflags/testflags.go
@@ -2,4 +2,5 @@ package testflags
 
 import "flag"
 
+// SkipCloudLogging is a flag of program arguments on testing.  Unit tests using Cloud Logging APIs are skipped with this flag.
 var SkipCloudLogging = flag.Bool("skip-cloud-logging", false, "skip tests that require cloud logging")
