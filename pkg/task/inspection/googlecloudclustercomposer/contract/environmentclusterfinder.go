fd3c664aeaed058ac656624c41294c4c27565621
kyasbal <ikakeru@google.com>
2025-10-15 18:17:15 +0900

    [feat #285 5-2/N] Replace API client to v2 for Cloud Composer cluster name auto completion (#296)
    * [feat #285 5-2/N] Replace API client to v2 for CloudComposer cluster name auto completion
    
    * fix issues pointed out by gemini-code-assist

---
+++ pkg/task/inspection/googlecloudclustercomposer/contract/environmentclusterfinder.go
@@ -0,0 +1,61 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package googlecloudclustercomposer_contract
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"cloud.google.com/go/container/apiv1/containerpb"
+	"github.com/GoogleCloudPlatform/khi/pkg/api/googlecloudv2"
+	coretask "github.com/GoogleCloudPlatform/khi/pkg/core/task"
+	googlecloudcommon_contract "github.com/GoogleCloudPlatform/khi/pkg/task/inspection/googlecloudcommon/contract"
+)
+
+var ErrEnvironmentClusterNotFound = errors.New("not found")
+
+type ComposerEnvironmentClusterFinder interface {
+	GetGKEClusterName(ctx context.Context, projectID, environment string) (string, error)
+}
+
+type EnvironmentClusterFinderImpl struct{}
+
+// GetGKEClusterName implements EnvironmentClusterFinder.
+func (e *EnvironmentClusterFinderImpl) GetGKEClusterName(ctx context.Context, projectID string, environment string) (string, error) {
+	cf := coretask.GetTaskResult(ctx, googlecloudcommon_contract.APIClientFactoryTaskID.Ref())
+
+	containerClusterManagerClient, err := cf.ContainerClusterManagerClient(ctx, googlecloudv2.Project(projectID))
+	if err != nil {
+		return "", err
+	}
+	defer containerClusterManagerClient.Close()
+
+	cluster, err := containerClusterManagerClient.ListClusters(ctx, &containerpb.ListClustersRequest{
+		Parent: fmt.Sprintf("projects/%s/locations/-", projectID),
+	})
+	if err != nil {
+		return "", err
+	}
+
+	for _, c := range cluster.Clusters {
+		if c.ResourceLabels["goog-composer-environment"] == environment {
+			return c.Name, nil
+		}
+	}
+	return "", ErrEnvironmentClusterNotFound
+}
+
+var _ ComposerEnvironmentClusterFinder = (*EnvironmentClusterFinderImpl)(nil)
