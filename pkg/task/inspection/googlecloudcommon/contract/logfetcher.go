69b20a04b93e2d420d8ecf042f24dc3964972439
kyasbal <ikakeru@google.com>
2025-10-17 16:29:40 +0900

    Enabled OAuth option with the client v2 (#315)
    * Enabled OAuth option with the client v2
    
    * fix issues pointed by gemini-code-review

--- pkg/task/inspection/googlecloudcommon/contract/logfetcher.go
+++ pkg/task/inspection/googlecloudcommon/contract/logfetcher.go
@@ -19,6 +19,7 @@ import (
 
 	logging "cloud.google.com/go/logging/apiv2"
 	"cloud.google.com/go/logging/apiv2/loggingpb"
+	"github.com/GoogleCloudPlatform/khi/pkg/api/googlecloudv2"
 	"google.golang.org/api/iterator"
 )
 
@@ -54,7 +55,7 @@ func (l *logFetcherImpl) FetchLogs(dest chan<- *loggingpb.LogEntry, ctx context.
 		Filter:        filter,
 		OrderBy:       l.orderBy,
 		PageSize:      l.pageSize,
-	})
+	}, googlecloudv2.DefaultRetryPolicy, googlecloudv2.NeverTimeout)
 
 	for {
 		entry, err := iter.Next()
