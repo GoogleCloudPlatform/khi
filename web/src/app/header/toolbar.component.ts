6ac02ec7bf4f51a2d79c75ac868177a575838685
Kakeru Ishii <ikakeru@google.com>
2026-01-26 09:26:12 +0900

    fix the access level issue for a method

--- web/src/app/header/toolbar.component.ts
+++ web/src/app/header/toolbar.component.ts
@@ -209,7 +209,7 @@ export class ToolbarComponent implements OnInit, OnDestroy {
   }
 
   @HostListener('window:keydown', ['$event'])
-  private interceptBrowserSearch(event: KeyboardEvent) {
+  protected interceptBrowserSearch(event: KeyboardEvent) {
     if (event.key === 'f' && (event.ctrlKey || event.metaKey)) {
       event.preventDefault();
       this.logFilterInput()?.focus();
