bea5705df3b01173477c1fab12b3ae92542e6a6d
Kakeru Ishii <ikakeru@google.com>
2026-03-31 12:11:50 +0900

    feat(web): migrate layout system to Golden Layout
    Replaced angular-split with Golden Layout to provide a more flexible and user-configurable workspace.
    - Removed `as-split` and added Golden Layout container in `MainComponent`.
    - Created `LayoutService` to manage component registration and layout configuration.
    - Added a custom theme matching KHI design system in `golden-layout-khi-theme.scss`.

---
+++ web/src/app/services/layout/layout.service.ts
@@ -0,0 +1,130 @@
+/**
+ * Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { ViewContainerRef } from '@angular/core';
+import { GoldenLayout, ComponentContainer, LayoutConfig } from 'golden-layout';
+import { TimelineSmartComponent } from '../../timeline/timeline-smart.component';
+import { LogSmartComponent } from '../../log/log-smart.component';
+import { DiffSmartComponent } from '../../diff/diff-smart.component';
+
+/**
+ * LayoutService manages the GoldenLayout instance and component registration.
+ */
+export class LayoutService {
+  /** The GoldenLayout instance. */
+  private goldenLayout!: GoldenLayout;
+
+  /** ViewContainerRef for creating Angular components dynamically. */
+  private viewContainerRef!: ViewContainerRef;
+
+  /** ResizeObserver to track container size changes. */
+  private resizeObserver?: ResizeObserver;
+
+  /** The default layout configuration used if no saved state is found. */
+  private readonly defaultLayout: LayoutConfig = {
+    settings: {
+      showPopoutIcon: false,
+    },
+    dimensions: {
+      borderWidth: 5,
+    },
+    root: {
+      type: 'row',
+      content: [
+        {
+          type: 'component',
+          componentType: 'timeline',
+          title: 'Timeline',
+          size: '70%',
+          isClosable: false,
+        },
+        {
+          type: 'component',
+          componentType: 'log',
+          title: 'Logs',
+          size: '15%',
+        },
+        {
+          type: 'component',
+          componentType: 'diff',
+          title: 'History',
+          size: '15%',
+        },
+      ],
+    },
+  };
+
+  /**
+   * Initialize GoldenLayout.
+   */
+  public init(hostElement: HTMLElement, vcr: ViewContainerRef) {
+    this.viewContainerRef = vcr;
+    this.goldenLayout = new GoldenLayout(hostElement);
+
+    this.registerComponents();
+
+    this.resizeObserver = new ResizeObserver(() => {
+      this.goldenLayout.setSize(
+        hostElement.clientWidth,
+        hostElement.clientHeight,
+      );
+    });
+    this.resizeObserver.observe(hostElement);
+  }
+
+  /**
+   * Register components to GoldenLayout.
+   */
+  private registerComponents() {
+    this.goldenLayout.registerComponentFactoryFunction(
+      'timeline',
+      (container: ComponentContainer) => {
+        const componentRef = this.viewContainerRef.createComponent(
+          TimelineSmartComponent,
+        );
+        container.element.appendChild(componentRef.location.nativeElement);
+        container.on('destroy', () => componentRef.destroy());
+      },
+    );
+
+    this.goldenLayout.registerComponentFactoryFunction(
+      'log',
+      (container: ComponentContainer) => {
+        const componentRef =
+          this.viewContainerRef.createComponent(LogSmartComponent);
+        container.element.appendChild(componentRef.location.nativeElement);
+        container.on('destroy', () => componentRef.destroy());
+      },
+    );
+
+    this.goldenLayout.registerComponentFactoryFunction(
+      'diff',
+      (container: ComponentContainer) => {
+        const componentRef =
+          this.viewContainerRef.createComponent(DiffSmartComponent);
+        container.element.appendChild(componentRef.location.nativeElement);
+        container.on('destroy', () => componentRef.destroy());
+      },
+    );
+  }
+
+  /**
+   * Load default layout configuration.
+   */
+  public loadDefaultLayout() {
+    this.goldenLayout.loadLayout(this.defaultLayout);
+  }
+}
