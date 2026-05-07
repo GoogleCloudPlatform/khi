9c6b3dbeca4880cc73dd79f3388c78a3905eacbb
kyasbal <ikakeru@google.com>
2026-05-07 11:27:04 +0900

    feat: Adding TimelineStyleRegistry (#639)

--- scripts/make/codegen.mk
+++ scripts/make/codegen.mk
@@ -23,8 +23,13 @@ $(GENERATE_BACKEND_DUMMY): ## Generate backend source code
 .PHONY: generate-backend
  generate-backend: $(GENERATE_BACKEND_DUMMY) ## Generate backend source code
 
+# TODO: eventually the following cp commands are not needed after we removed icon image dependency directly from the frontend.
 $(FRONTEND_GENERATED_ASSETS_DUMMY): scripts/msdf-generator/index.js scripts/msdf-generator/zzz_generated_used_icons.json $(MSDF_SETUP_DUMMY)## Generate font atlas
 	cd scripts/msdf-generator && node index.js
+	mkdir -p pkg/model/khifile/v6/style/assets
+	cp web/src/assets/zzz-icon-codepoints.json pkg/model/khifile/v6/style/assets/
+	cp web/src/assets/zzz-material-icons-msdf.json pkg/model/khifile/v6/style/assets/
+	cp web/src/assets/zzz-material-icons-msdf.png pkg/model/khifile/v6/style/assets/
 	touch $(FRONTEND_GENERATED_ASSETS_DUMMY)
 
 generate-frontend-assets: $(FRONTEND_GENERATED_ASSETS_DUMMY) ## Generate font atlas
