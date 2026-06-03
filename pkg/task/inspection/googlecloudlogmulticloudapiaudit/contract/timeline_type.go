81e63910c8bbd49ddabd187a44f292ef692424fa
kyasbal <ikakeru@google.com>
2026-06-03 13:43:55 +0900

    Migrate GKE multicloud audit log parsers to file format v6 (#727)

---
+++ pkg/task/inspection/googlecloudlogmulticloudapiaudit/contract/timeline_type.go
@@ -0,0 +1,70 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package googlecloudlogmulticloudapiaudit_contract
+
+import (
+	pb "github.com/GoogleCloudPlatform/khi/pkg/generated/khifile/v6"
+	"github.com/GoogleCloudPlatform/khi/pkg/model/khifile/v6/style"
+)
+
+var (
+	// TimelineTypeProject is the timeline type for a Google Cloud project.
+	TimelineTypeProject = style.MustRegisterTimelineType(
+		"project",
+		"Google Cloud Project",
+		"cloud",
+		0.6,
+		style.MustForceConvertSRGBHex("#111111"),
+		style.ColorWhite,
+		style.MustForceConvertSRGBHex("#F5F5F5"),
+		true,
+		50,
+	)
+
+	// TimelineTypeMultiCloudCluster is the timeline type style for MultiCloud Cluster resources.
+	TimelineTypeMultiCloudCluster = style.MustRegisterTimelineType(
+		"multicloudCluster",
+		"MultiCloud Cluster",
+		"dns",
+		1.0,
+		style.ColorWhite,
+		style.ColorBlack,
+		style.MustForceConvertSRGBHex("#4285F4"),
+		true,
+		1000,
+	)
+
+	// TimelineTypeMultiCloudNodepool is the timeline type style for MultiCloud Nodepool resources.
+	TimelineTypeMultiCloudNodepool = style.MustRegisterTimelineType(
+		"multicloudNodepool",
+		"MultiCloud NodePool",
+		"list",
+		1.0,
+		style.ColorWhite,
+		style.ColorBlack,
+		style.MustForceConvertSRGBHex("#34A853"),
+		true,
+		1100,
+	)
+
+	// RevisionStateProvisioning is the style for a resource that is being provisioned.
+	RevisionStateProvisioning = style.MustRegisterRevisionState(
+		"Resource is being provisioned",
+		"deployed_code_history",
+		"Resource is being provisioned",
+		style.MustForceConvertSRGBHex("#6666ff"),
+		pb.RevisionStateStyle_REVISION_STATE_STYLE_NORMAL,
+	)
+)
