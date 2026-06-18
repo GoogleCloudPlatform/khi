a48999f53d0216da34d5720c80e994d8315401ee
ikakeru <ikakeru@google.com>
2026-06-18 09:25:59 +0900

    bug: fix CSM's access log query was wrong for GDC clusters

--- pkg/task/inspection/googlecloudlogonpremapiaudit/impl/query_task_test.go
+++ pkg/task/inspection/googlecloudlogonpremapiaudit/impl/query_task_test.go
@@ -31,10 +31,15 @@ func TestGenerateOnPremAPIQuery(t *testing.T) {
 		{
 			name: "BaremetalCluster",
 			cluster: googlecloudk8scommon_contract.GoogleCloudClusterIdentity{
-				ProjectID:         "test-project",
-				ClusterName:       "test-cluster",
-				ClusterTypePrefix: "baremetalClusters/",
-				Location:          "asia-northeast1",
+				ProjectID:   "test-project",
+				ClusterName: "test-cluster",
+				PrefixPolicy: googlecloudk8scommon_contract.ClusterPrefixPolicy{
+					Prefix: "baremetalClusters/",
+					RequiredUsages: []googlecloudk8scommon_contract.ClusterNameUsage{
+						googlecloudk8scommon_contract.ClusterNameUsageK8sPlatformAudit,
+					},
+				},
+				Location: "asia-northeast1",
 			},
 			want: `
 log_id("cloudaudit.googleapis.com/activity") OR log_id("cloudaudit.googleapis.com/data_access")
@@ -65,10 +70,15 @@ func TestGenerateOnPremAPIQueryIsValid(t *testing.T) {
 		{
 			name: "Valid Query",
 			cluster: googlecloudk8scommon_contract.GoogleCloudClusterIdentity{
-				ProjectID:         "test-project",
-				ClusterName:       "test-cluster",
-				ClusterTypePrefix: "baremetalClusters/",
-				Location:          "asia-northeast1",
+				ProjectID:   "test-project",
+				ClusterName: "test-cluster",
+				PrefixPolicy: googlecloudk8scommon_contract.ClusterPrefixPolicy{
+					Prefix: "baremetalClusters/",
+					RequiredUsages: []googlecloudk8scommon_contract.ClusterNameUsage{
+						googlecloudk8scommon_contract.ClusterNameUsageK8sPlatformAudit,
+					},
+				},
+				Location: "asia-northeast1",
 			},
 		},
 	}
